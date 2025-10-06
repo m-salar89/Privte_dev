9d09352ca96f51b302db2bb0ed88305d3a862968
m_salar89 <salar.maow@zubler.de>
2025-10-06 11:33:13 +0200

    test

--- src/lambda_funktion.py
+++ src/lambda_funktion.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dump("hat es geklappt jetzt?")
+        'body': json.dump("???????????")
     }
