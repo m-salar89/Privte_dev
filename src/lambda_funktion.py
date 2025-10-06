273f211cd34f5dd1ddcb05dd6073c7cbf19a90ab
m_salar89 <salar.maow@zubler.de>
2025-10-06 11:41:44 +0200

    repariert

--- src/lambda_funktion.py
+++ src/lambda_funktion.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dump("???????????")
+        'body': json.dump("jetzt soltte klappen !!")
     }
